c20cbae02ee946b9ad9c3eb008c173d0665f1e0a
Axerion1210 <octavianusgiovanni@gmail.com>
2021-01-08 12:24:16 +0700

    add Type UFO

--- Assets/Scripts/ShootableTarget.cs
+++ Assets/Scripts/ShootableTarget.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ShootableTarget : MonoBehaviour
 {
     public int currentHealth = 1;
+    public int typeUFO;
+    void Start()
+    {
+        typeUFO = Random.Range(1, 10);
+        if (typeUFO >= 1 && typeUFO <= 6)
+            currentHealth = 3;
+        else if (typeUFO >= 7 && typeUFO <= 8)
+            currentHealth = 5;
+        else if (typeUFO >= 9 && typeUFO <= 10)
+            currentHealth = 1;
+    }
 
     public void Damage(int damageAmount)
     {
